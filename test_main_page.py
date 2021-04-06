150e39c233c9089f01da36152bc81ddb97f8863e
OlhaHorodniuk <osb2605@gmail.com>
2021-04-06 13:59:13 +0300

    Initializing LoginPage for implicit page transition

--- test_main_page.py
+++ test_main_page.py
@@ -5,8 +5,7 @@ from pages.login_page import LoginPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
-    page_login = LoginPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    page_login = LoginPage(browser, browser.current_url)
     page_login.should_be_login_page()
