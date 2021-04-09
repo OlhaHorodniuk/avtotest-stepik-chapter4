dfefa32060e88e210072273697350191703e7f61
OlhaHorodniuk <osb2605@gmail.com>
2021-04-09 11:20:26 +0300

    refactoring code before review

--- test_product_page.py
+++ test_product_page.py
@@ -25,9 +25,8 @@ class TestUserAddToBasketFromProductPage:
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
-    @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-    def test_user_can_add_product_to_basket(self, browser, promo_offer):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    def test_user_can_add_product_to_basket(self, browser):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
@@ -37,9 +36,8 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+def test_guest_can_add_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
